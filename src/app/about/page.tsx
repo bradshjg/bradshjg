d5cc08af9d0c3be13f1f105e201d4c73bd15a771
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2023-07-27 22:26:28 -0500

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main className="mt-8 flex items-center justify-around">
+        <p>
+          This is the about page!
+        </p>
+    </main>
+  )
+}
